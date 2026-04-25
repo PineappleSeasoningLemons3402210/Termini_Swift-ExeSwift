5b0f6b40de0e845490112821f85528ecef5c107a
PineappleSeasoningLemons3402210 <AirChip3650@outlook.com>
2026-04-25 11:01:31 +0800

    Create HelpCommand.swift

---
+++ HelpCommand.swift
@@ -0,0 +1,14 @@
+struct HelpCommand {
+    static func run() -> String {
+        return """
+        Termini Shell
+        
+        Commands:
+        ls      List files
+        cd      Change directory
+        pwd     Show current path
+        echo    Print text
+        help    Show this message
+        """
+    }
+}
